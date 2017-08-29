734685789d48b737e919ad72e6883ed081fa14e7
Nahuel Banos <nahuelb@gmail.com>
2017-08-29 10:55:35 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,9 @@
+function forloop(){
+  for (var i = 0; i <= 25; i++){
+	   if (i===1) {
+		     console.log("I have " + i + "strange loope")
+       } else {
+		     console.log("I have" + i + "strange Loopes")
+       }
+     }
+}
